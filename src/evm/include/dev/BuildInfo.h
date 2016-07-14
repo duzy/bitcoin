b516415dab3e9cb179b463426dfdbb6cb5805f84
Duzy Chan <code@duzy.info>
2016-07-14 17:30:46 +0800

    Integrates Quantum EVM libraries (see src/evm/lib*) -- we're just building `libdevcore`, `libevm`, `libevmcore`, a lot more to be done to build all libs.

---
+++ src/evm/include/dev/BuildInfo.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define ETH_PROJECT_VERSION "0.0.1"
+#define ETH_COMMIT_HASH 0
+#define ETH_CLEAN_REPO 0
+#define ETH_BUILD_TYPE "qtum"
+#define ETH_BUILD_OS "Linux"
+#define ETH_BUILD_COMPILER "g++"
+#define ETH_BUILD_JIT_MODE "JIT"
+#define ETH_BUILD_PLATFORM "Linux/g++/JIT"
+#define ETH_BUILD_NUMBER 1
+#define ETH_VERSION_SUFFIX "eval"
